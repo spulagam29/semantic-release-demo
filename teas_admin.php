f9379c59f3ea9e7d31b6248e8c38293c6bd409d9
Suseela Reddy Pulagam <spulagam@evoketechnologies.com>
2026-02-05 16:14:28 +0530

    test

--- teas_admin.php
+++ teas_admin.php
@@ -34,7 +34,7 @@ $result = $conn->query('SELECT id, Name, Type, Origin, Strength, Price, ImageURL
         }
 
         nav {
-            margin-top: 50px;
+            margin-top: 51px;
             background: #cdffcd;
             display: flex;
             justify-content: space-between;
